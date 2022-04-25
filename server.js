60a92b88cf63f0fb03e01dae20e5603622144e26
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-25 16:58:25 +0300

    return Procfile

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./src/db.json');
-const middleware = jsonServer.defaults({ static: './build' });
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({ static: './dist' });
 const PORT = process.env.PORT || 5000;
-server.use(middleware);
+server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
