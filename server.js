dcfc8c50f8d97324022425bcf912f70e4c715207
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-25 14:17:31 +0300

    fix server.js

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const router = jsonServer.router('./src/db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8000;
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  })
-);
 server.use(router);
 server.listen(PORT, () => {
   console.log('Server is running');
