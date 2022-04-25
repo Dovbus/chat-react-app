b9262979449ae998bf8841e77779cb178a7b462c
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-25 12:32:47 +0300

    changes

--- src/redux/currentUserSlice.js
+++ src/redux/currentUserSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
 
-const URL_USER = 'http://localhost:3002/currentUser';
+const URL_USER = 'https://chat-test-react-app.herokuapp.com/currentUser';
 
 export const loadCurrentUser = createAsyncThunk(
   'currentUser/loadCurrentUser',
