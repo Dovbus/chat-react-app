52139216432989d0d0ba7e51bafa1282de78e19c
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-25 16:25:42 +0300

    fixes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./src/db.json');
-const middlewares = jsonServer.defaults({ static: './build' });
+const middleware = jsonServer.defaults({ static: './build' });
 const PORT = process.env.PORT || 5000;
-server.use(middlewares);
+server.use(middleware);
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
