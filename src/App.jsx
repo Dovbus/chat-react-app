3625893daea31cb265772b5de041281671d971dd
Olia Dovbush <oliaivanchushun@gmail.com>
2022-04-25 19:20:55 +0300

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
